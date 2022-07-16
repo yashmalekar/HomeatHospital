e6b4ce36a94f13ac97b79b5a488a9c2bae16c2ba
yash_malekar <heramb.malekar@gmail.com>
2022-07-16 13:16:51 +0530

    config updated

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const api_config = {
-    port: 5000,
+    port: process.env.PORT||5000,
     db_url: 'mongodb+srv://yash:yash@cluster0.dwgal.mongodb.net/homehospital?retryWrites=true&w=majority',
     //db_url: 'mongodb+srv://HBBD:BBD1234@cluster0.gkwv4.mongodb.net/homehospital?retryWrites=true&w=majority',
     stripe_sk: "sk_test_51InFnsSJ9UKCtISEiea1NalXQI6JTdeUMXKfeSgyUNq5FV8Dn57uyNqN48wUjoRknQa3fNUcNiVyKjIBswJvkx8M00441YKrRk",
